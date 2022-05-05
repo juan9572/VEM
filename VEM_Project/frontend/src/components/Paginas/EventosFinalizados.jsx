6ce3c7ddd068d228aaecabc2fe1911dbae875487
Isaac <isaactadinagiraldo02@gmail.com>
2022-05-05 09:01:16 -0500

    Eventos finalizados componente

--- VEM_Project/frontend/src/components/Paginas/EventosFinalizados.jsx
+++ VEM_Project/frontend/src/components/Paginas/EventosFinalizados.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {useEffect, useState} from 'react';
 import {Room,Star} from "@material-ui/icons";
 
 import axios from 'axios';
+
+export default class EventosFinalizados extends Component {
+    render(){
+        return(
+            <div>
+                <br/>
+                <h2>This is the 'Eventos Finalizados' page</h2>
+                <br/>
+            </div>
+        )
+    }
+}
